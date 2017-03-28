d23424dc699d1fc91873b4fc01a6c3dc2ff0a808
Reginaldo Junior <76regi@gmail.com>
2017-03-28 01:02:27 -0300

    add docs and site

--- firstTeste/urls.py
+++ firstTeste/urls.py
@@ -4,14 +4,17 @@ from core.views import home
 from rest_framework import routers, serializers, viewsets
 from core.models import Employee, Andress
 from views import EmployeeViewSet, AndressViewSet
+from rest_framework_swagger.views import get_swagger_view
 
-# Routers provide an easy way of automatically determining the URL conf.
+schema_view = get_swagger_view(title='Pastebin API')
+
 router = routers.DefaultRouter()
 
 router.register(r'employee', EmployeeViewSet)
 router.register(r'andress', AndressViewSet)
 
 urlpatterns = [
+    url(r'^docs/', schema_view),
     url(r'^api/', include(router.urls)),
     url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
