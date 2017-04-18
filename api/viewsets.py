effddf3bfcecbf1efe0994a64280c08a800c8db2
Reginaldo Junior <76regi@gmail.com>
2017-04-18 20:41:39 -0300

    formatter code style pep8 #1

---
+++ api/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework import routers, serializers, viewsets
+from core.models import Employee
+from serializers import EmployeeSerialize
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerialize
