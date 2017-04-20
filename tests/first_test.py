142ce017f4bc9b393d3e7b07ca91073686dc22d6
Reginaldo Junior <reginaldo.junior@thirdlevel.com.br>
2017-04-20 15:37:10 -0300

    first test

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,7 +1,3 @@
 
-def test_answer(cmdopt):
-    if cmdopt == "type1":
-        print ("first")
-    elif cmdopt == "type2":
-        print ("second")
-    assert 0 # to see what was printed
+def test_answer():
+    assert 0 == 0 # to see what was printed
