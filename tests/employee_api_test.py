784f2b04c0aff7fb3f4a262c64d2115a2684129a
Reginaldo Junior <reginaldo.junior@thirdlevel.com.br>
2017-04-20 19:04:14 -0300

    add tests to employee

---
+++ tests/employee_api_test.py
@@ -0,0 +1,56 @@
+import requests
+import pytest
+import json
+
+
+@pytest.fixture
+def temporary_employee():
+    from core.models import Employee
+
+    employee = Employee(
+        name="Reginaldo",
+        email="76regi@gmail.com",
+        department="TI")
+    employee.save()
+
+    return employee
+
+
+@pytest.mark.django_db
+def test_update(client, temporary_employee):
+    from core.models import Employee
+
+    response = client.post('/api/employee/{}/'.format(temporary_employee.id),
+                           json.dumps({"name": "Reginaldo Editado"}),
+                           HTTP_X_METHOD_OVERRIDE='PUT',
+                           content_type='application/json')
+
+    found_employee = Employee.objects.filter(name="Reginaldo")
+
+    assert "Reginaldo" not in found_employee
+
+
+@pytest.mark.django_db
+def test_delete(client, temporary_employee):
+    from core.models import Employee
+
+    response = client.post('/api/employee/{}/'.format(temporary_employee.id),
+                           json.dumps({"id": temporary_employee.id}),
+                           HTTP_X_METHOD_OVERRIDE='DELETE',
+                           content_type='application/json')
+
+    found_employee = Employee.objects.filter(name="Reginaldo")
+
+    assert "Reginaldo" not in found_employee
+
+
+@pytest.mark.django_db
+def test_get(client, temporary_employee):
+    response = client.get('/api/employee/{}/'.format(temporary_employee.id))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_get_not_found(client, temporary_employee):
+    response = client.get('/api/employee/46545456/')
+    assert response.status_code == 404
--- tests/first_test.py
+++
@@ -1,3 +0,0 @@
-
-def test_answer():
-    assert 0 == 0 # to see what was printed
